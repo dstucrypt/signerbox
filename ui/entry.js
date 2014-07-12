70161c6a4a2d54447386b10bea2a8f671da7223b
Ilya Petrov <ilya.muromec@gmail.com>
2014-07-12 18:03:23 +0300

    fix signerbox entry

--- ui/entry.js
+++ ui/entry.js
@@ -2,8 +2,8 @@ var ko = require('knockout'),
     UiMain = require('./uimain.js'),
     ui_main;
 
-var setup = function (root_node, nonce) {
-    ui_main = new UiMain(nonce); 
+var setup = function (root_node, nonce, csrf, domain) {
+    ui_main = new UiMain(nonce, csrf, domain);
     ko.applyBindings(ui_main, root_node);
 };
 
